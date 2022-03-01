26a198261feecae09848f5558a1fe4b064a7f413
maen08 <2001stany@gmail.com>
2022-03-01 20:17:48 +0300

    production-ready status

---
+++ setup.py
@@ -0,0 +1,54 @@
+# a setup file for pypi packages and librabr
+
+# Third Party
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt') as requirements_production:
+    install_requires = requirements_production.readlines()
+
+setup(
+    name='pyshoket',
+
+    description='A Python wrapper for Shoket API',
+
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
+
+    version='0.0',    # keep on changing
+
+    url='https://github.com/maen08/',
+
+    download_url='https://github.com/maen08/pyshoket/archive/refs/tags/v0.0.tar.gz',
+
+    author='Stanley Ruheza',
+
+    author_email='2001stany@gmail.com',
+
+    license='MIT',
+
+    packages=['pyshoket'],
+
+    include_package_data=True,
+
+    install_requires=install_requires,
+
+    keywords=['shoket', 'online payment', 'python'],
+
+    classifiers=[
+       
+        'Development Status :: 3 - Alpha',  # keep on increasing
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+    
+
+)
